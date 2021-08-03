edbe99d1a0bc44f58016b74d9d53b788b096b943
mishkayaremtso <49525882+mishkayaremtso@users.noreply.github.com>
2021-08-03 13:49:01 +0300

    Create projeqtor_post.py

---
+++ conf/projeqtor_post.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+driver.get("http://proj.eba-n2jnhybi.eu-central-1.elasticbeanstalk.com/view/main.php")
+DBHost = driver.find_element_by_id("param[DbHost]")
+DBHost.clear()
+DBHost.send_keys("terraform-20210802172036501700000002.cruyyjqp2wru.eu-central-1.rds.amazonaws.com")
+DBUser = driver.find_element_by_id("param[DbUser]")
+DBUser.clear()
+DBUser.send_keys("kek")
+DBPassword = driver.find_element_by_id("param[DbPassword]")
+DBPassword.send_keys("password")
+driver.find_element_by_id("configButton_label").click()
+driver.close()
