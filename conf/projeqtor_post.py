41cefaf1cbdb77b4873f863983aacd21db9ef1a8
mishkayaremtso <49525882+mishkayaremtso@users.noreply.github.com>
2021-08-04 13:48:47 +0300

    Update projeqtor_post.py

--- conf/projeqtor_post.py
+++ conf/projeqtor_post.py
@@ -4,10 +4,10 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-driver.get("http://proj.eba-n2jnhybi.eu-central-1.elasticbeanstalk.com/view/main.php")
+driver.get("proj.eba-d7suzmxn.eu-central-1.elasticbeanstalk.com")
 DBHost = driver.find_element_by_id("param[DbHost]")
 DBHost.clear()
-DBHost.send_keys("terraform-20210802172036501700000002.cruyyjqp2wru.eu-central-1.rds.amazonaws.com")
+DBHost.send_keys("terraform-20210804081622768900000002.cruyyjqp2wru.eu-central-1.rds.amazonaws.com")
 DBUser = driver.find_element_by_id("param[DbUser]")
 DBUser.clear()
 DBUser.send_keys("kek")
