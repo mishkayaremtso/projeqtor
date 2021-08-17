6db9154affa1107f21fbd38294d957c956f9f1b1
mishkayaremtso <49525882+mishkayaremtso@users.noreply.github.com>
2021-08-17 15:09:35 +0300

    Update projeqtor_post.py

--- conf/projeqtor_post.py
+++ conf/projeqtor_post.py
@@ -4,14 +4,14 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-driver.get("http://proj.eba-d7suzmxn.eu-central-1.elasticbeanstalk.com/view/main.php")
+driver.get("http://fff-projeqtor.azurewebsites.net")
 DBHost = driver.find_element_by_id("param[DbHost]")
 DBHost.clear()
-DBHost.send_keys("terraform-20210804081622768900000002.cruyyjqp2wru.eu-central-1.rds.amazonaws.com")
+DBHost.send_keys("azure-mysql-serv.mysql.database.azure.com")
 DBUser = driver.find_element_by_id("param[DbUser]")
 DBUser.clear()
 DBUser.send_keys("kek")
 DBPassword = driver.find_element_by_id("param[DbPassword]")
-DBPassword.send_keys("password")
+DBPassword.send_keys("HelloPassword")
 driver.find_element_by_id("configButton_label").click()
 driver.close()
