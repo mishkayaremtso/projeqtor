92cdceb0dcf15fbc7df26a2cc48ce136af9ee7b5
mishkayaremtso <49525882+mishkayaremtso@users.noreply.github.com>
2021-08-17 18:12:18 +0300

    Update projeqtor_post.py

--- conf/projeqtor_post.py
+++ conf/projeqtor_post.py
@@ -4,10 +4,10 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-driver.get("http://fff-projeqtor.azurewebsites.net/view/main.php")
+driver.get("http://projeqtor.azurewebsites.net/view/main.php")
 DBHost = driver.find_element_by_id("param[DbHost]")
 DBHost.clear()
-DBHost.send_keys("azure-mysql-serv.mysql.database.azure.com")
+DBHost.send_keys("mysql-kekdb.mysql.database.azure.com")
 DBUser = driver.find_element_by_id("param[DbUser]")
 DBUser.clear()
 DBUser.send_keys("kek")
